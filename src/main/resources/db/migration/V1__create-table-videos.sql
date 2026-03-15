93322211437b78712bbbcc767e3cb3872303c5c7
lucasmoraist <luksmnt1101@gmail.com>
2026-03-15 10:44:30 -0300

    feat: add database configuration and initial migration scripts

---
+++ src/main/resources/db/migration/V1__create-table-videos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE t_lf_video (
+    id bigint(20) NOT NULL AUTO_INCREMENT,
+    title varchar(255) NOT NULL,
+    description TEXT,
+    url varchar(500) NOT NULL,
+    PRIMARY KEY (id)
+);
