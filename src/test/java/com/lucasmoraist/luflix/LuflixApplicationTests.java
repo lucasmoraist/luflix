7ed39049c3c81c1cd06b50ce473b59b6c99d8537
lucasmoraist <luksmnt1101@gmail.com>
2026-03-15 10:20:59 -0300

    first commit

---
+++ src/test/java/com/lucasmoraist/luflix/LuflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.luflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
