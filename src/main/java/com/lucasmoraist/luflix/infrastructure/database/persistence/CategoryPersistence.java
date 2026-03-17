cc30846007783ec8969df6090bcc87354179035c
lucasmoraist <luksmnt1101@gmail.com>
2026-03-17 00:26:58 -0300

    feat: implement CategoryPersistence for managing category entities in the database

---
+++ src/main/java/com/lucasmoraist/luflix/infrastructure/database/persistence/CategoryPersistence.java
@@ -0,0 +1,46 @@
+package com.lucasmoraist.luflix.infrastructure.database.persistence;
+
+import com.lucasmoraist.luflix.domain.model.Category;
+import com.lucasmoraist.luflix.infrastructure.api.web.request.CategoryRequest;
+import com.lucasmoraist.luflix.infrastructure.database.entity.CategoryEntity;
+import com.lucasmoraist.luflix.infrastructure.database.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryPersistence {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryPersistence(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public void delete(CategoryEntity videoEntity) {
+        this.categoryRepository.delete(videoEntity);
+    }
+
+    public List<CategoryEntity> findAll() {
+        return this.categoryRepository.findAll();
+    }
+
+    public Optional<CategoryEntity> findById(Long videoId) {
+        return this.categoryRepository.findById(videoId);
+    }
+
+    public Category save(CategoryRequest request) {
+        CategoryEntity entity = CategoryEntity.builder()
+                .title(request.title())
+                .color(request.color())
+                .build();
+        CategoryEntity savedEntity = this.categoryRepository.save(entity);
+        return Category.toDomain(savedEntity);
+    }
+
+    public void update(CategoryEntity categoryEntity) {
+        this.categoryRepository.save(categoryEntity);
+    }
+
+}
