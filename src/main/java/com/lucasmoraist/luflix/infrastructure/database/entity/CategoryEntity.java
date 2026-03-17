f13fac0bca87e2323e7a7f00a28ad88d30830f06
lucasmoraist <luksmnt1101@gmail.com>
2026-03-17 00:26:24 -0300

    feat: add update method and toEntity static method to CategoryEntity for improved data handling

--- src/main/java/com/lucasmoraist/luflix/infrastructure/database/entity/CategoryEntity.java
+++ src/main/java/com/lucasmoraist/luflix/infrastructure/database/entity/CategoryEntity.java
@@ -1,5 +1,6 @@
 package com.lucasmoraist.luflix.infrastructure.database.entity;
 
+import com.lucasmoraist.luflix.infrastructure.api.web.request.CategoryRequest;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,5 +23,18 @@ public class CategoryEntity {
     private Long id;
     private String title;
     private String color;
+
+    public void update(CategoryRequest request) {
+        if (request.title() != null) this.title = request.title();
+        if (request.color() != null) this.color = request.color();
+    }
+
+    public static CategoryEntity toEntity(CategoryRequest category) {
+        return new CategoryEntity(
+                null,
+                category.title(),
+                category.color()
+        );
+    }
 
 }
