a4ad324cf09576797d082985676664f3c7ade284
lucasmoraist <luksmnt1101@gmail.com>
2026-03-20 19:26:17 -0300

    feat: enhance FindAllVideoResponse to include category information

--- src/main/java/com/lucasmoraist/luflix/infrastructure/api/web/response/video/FindAllVideoResponse.java
+++ src/main/java/com/lucasmoraist/luflix/infrastructure/api/web/response/video/FindAllVideoResponse.java
@@ -1,18 +1,23 @@
 package com.lucasmoraist.luflix.infrastructure.api.web.response.video;
 
+import com.lucasmoraist.luflix.domain.model.Category;
 import com.lucasmoraist.luflix.infrastructure.database.entity.VideoEntity;
 
 public record FindAllVideoResponse(
         Long id,
         String title,
-        String url
+        String url,
+        Category category
 ) {
 
     public static FindAllVideoResponse toResponse(VideoEntity entity) {
         return new FindAllVideoResponse(
                 entity.getId(),
                 entity.getTitle(),
-                entity.getUrl()
+                entity.getUrl(),
+                entity.getCategory() != null
+                        ? Category.toDomain(entity.getCategory())
+                        : null
         );
     }
 
